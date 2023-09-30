fd271ad6bdb91eb8d292beecd53d38a40d41c341
Adeleke Moyinoluwa Solomon <moyinadeleke@yahoo.com>
2023-09-30 12:15:39 +0100

    added email verification, forgot password and reset password routes

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import LogIn from "../pages/LogIn";
 import SignUp from "../pages/SignUp";
+import EmailVerify from "../pages/EmailVerify";
+import ResetPassword from "../pages/ResetPassword";
+import ForgotPassword from "../pages/ForgotPassword";
 
 export const NavRoutes = () => {
   return (
@@ -11,6 +14,9 @@ export const NavRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/register" element={<SignUp />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/email-verification" element={<EmailVerify />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
   );
