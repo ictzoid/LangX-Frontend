bbe48e617289b0e7c0d785c73d21c88ea2ba94e6
Adeleke Moyinoluwa Solomon <moyinadeleke@yahoo.com>
2023-10-01 15:03:39 +0100

    set up protected route

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
 import LogIn from "../pages/LogIn";
 import SignUp from "../pages/SignUp";
 import EmailVerify from "../pages/EmailVerify";
 import ResetPassword from "../pages/ResetPassword";
 import ForgotPassword from "../pages/ForgotPassword";
-import Notfound from "../pages/NotFound";
+import NotFound from "../pages/NotFound";
+import ProtectedRoutes from "./ProtectedRoutes";
+import Dashboard from "../pages/Dashboard";
 
 export const NavRoutes = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LogIn />} />
+        <Route path="/" element={<LogIn />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/email-verification" element={<EmailVerify />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="*" element={<Notfound />} />
+
+        <Route element={<ProtectedRoutes />} isAuthenticated={isAuthenticated}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
