b18dd43935a50de4163aa3a3bdcd5cf1e9406d63
dedayorr <dedayo16@gmail.com>
2023-10-03 13:27:45 +0100

    button

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
 import LogIn from "../pages/LogIn";
 import SignUp from "../pages/SignUp";
 import EmailVerify from "../pages/EmailVerify";
@@ -12,8 +11,7 @@ export const NavRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LogIn />} />
+        <Route path="/" element={<LogIn />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/email-verification" element={<EmailVerify />} />
