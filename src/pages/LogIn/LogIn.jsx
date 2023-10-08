ac7667f8bc4fb73359807b7e6f64d8f870f7e989
Eni-iyi~ <akinbuliabraham@gmail.com>
2023-10-08 19:56:30 +0100

    input collection update

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import heroImg from "../../assets/images/Hero Image.png";
-import google from "../../assets/images/google.png";
-import apple from "../../assets/images/apple.png";
-import { FiEye, FiEyeOff } from "react-icons/fi";
+import googleIcon from "../../assets/images/google.png";
+import appleIcon from "../../assets/images/apple.png";
+// import { FiEye, FiEyeOff } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
 import { showToast } from "../../Toastify/Toast";
+import { MdEmail } from "react-icons/md";
+import Input from "../../components/Input";
+import { PrimaryBtn } from "../../components/Buttons/PrimaryBtn";
+import { SignUpButton } from "../../components/Buttons/SignUpBtn";
 
 export const LogIn = () => {
-  const [view, setView] = useState(false);
+  
   const [isLoading, setIsLoading] = useState(false);
   const [login, setLogin] = useState({
-  
     email: "",
     password: "",
   });
@@ -23,15 +26,11 @@ export const LogIn = () => {
       [name]: value,
     });
   };
-  // console.log("login", login)
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    if (
-    
-      login.email === "" || 
-      login.password === ""
-      ) {
+    if (login.email === "" || login.password === "") {
       showToast({
         message: "Please fill all fields",
         type: "error",
@@ -45,104 +44,87 @@ export const LogIn = () => {
         });
         setLogin({
           email: "",
-          password: ""
+          password: "",
         });
         setIsLoading(false);
       }, 2000);
     }
   };
   return (
-    <div className="flex justify-center max-w-[1300px] mx-auto gap-80">
-      <div className="hidden lg:block">
-        <img className="w-[149.1px] h-[36px] mt-3" src={Logo} />
-        <img className="w-[300px] h-[600px]" src={heroImg} />
+    <div className="flex justify-center w-[90%] mx-auto">
+      
+      <div className="hidden lg:flex lg:items-center lg:justify-center lg:flex-col w-[50%]">
+      <img className="w-[149.1px] h-[36px] ml-[-200px] mt-3" src={Logo} />
+        <img className="w-[60%] h-[600px] pr-[20px]" src={heroImg} />
       </div>
-      <div className="text-center mt-[60px] border-l">
+      <div className="text-center pt-[60px] lg:border-l lg:w-[45%] px-[24px]">
         <h2 className="font-semibold leading-[60px] text-[40px]">
           Welcome back!
         </h2>
-        <div className="font-normal text-[16px] leading-[22px] text-center text-[#616161] w-[364px] h-[88px]">
+        <div className="font-normal text-[16px] leading-[22px] text-center text-GrayColor1 88px]">
           <p>Unlock the world of languages and embark on </p>
           <p>an extraordinary experience.</p>
           <p>Login now and discover the power of</p>
           <p>communication.</p>
         </div>
         <div className="container">
-          <div className="flex w-[366px] h-[42px] border-2 text-[#713586] p-[20px, 48px] gap-8 justify-center items-center rounded-lg mt-6 cursor-pointer">
-            <img className="mr-3" src={google} />
-            Continue with Google
-          </div>
-          <div className="flex w-[366px] h-[42px] text-[#713586] p-[20px, 48px] border-2 justify-center items-center rounded mt-4 gap-8 cursor-pointer">
-            {" "}
-            <img className="mr-3" src={apple} />
-            Continue with Apple
-          </div>
+        <div className="flex flex-col mt-6 gap-4">
+              <SignUpButton icon={googleIcon} text="Sign up with Google" />
+              <SignUpButton icon={appleIcon} text="Sign up with Apple" />
+            </div>
+        
           <div className="mt-3 flex items-center gap-2">
             <div className="flex-grow border-t border-gray-300"></div>
             <span>or</span>
             <div className="flex-grow border-t border-gray-300"></div>
           </div>
           <div>
-            <input
-              className="w-[366px] h-[42px] px-3 gap-4 outline-none border-2 rounded mt-4"
-              type="text"
-              id="email"
+            <Input
+              type="email"
+              className="border border-GrayColor1 mt-4 w-full py-3 rounded-[7px] px-[8px] text-[12px] md:text-[14px] lg:text-[16px] outline-none"
+              placeholder="Email address"
               name="email"
+              id="email"
               value={login.email}
               onChange={handleLogin}
-              placeholder="Username/Email"
+              rightIcon={<MdEmail color= "primaryColor" />}
             />
           </div>
-          <div className=" flex items-center border-2 w-[366px] h-[42px] mt-4">
-            <input
-              className="w-[90%] outline-none px-3"
-              type={view ? "text" : "password"}
-              id="password"
+          <div>
+            <Input
+              className="border border-GrayColor1 w-full py-3 mt-3 rounded-[7px] px-[8px] text-[12px] md:text-[14px] lg:text-[16px] outline-none"
+              placeholder="Password"
               name="password"
+              id="password"
               value={login.password}
               onChange={handleLogin}
-              placeholder="Password"
+              type="password"
             />
-            {view ? (
-              <FiEye
-                className="cursor-pointer"
-                onClick={() => {
-                  setView(false);
-                }}
-              />
-            ) : (
-              <FiEyeOff
-                className="cursor-pointer"
-                onClick={() => {
-                  setView(true);
-                }}
-              />
-            )}
           </div>
-          <div className=" flex  items-center gap-9 w-[369px] h-[29px] mt-1">
+          <div className=" flex items-center justify-between h-[29px] mt-1">
             <div>
-              <input className="w-[20px] h-[15px] border-2" type="checkbox" />
-              <label>Keep me logged in</label>
+              <input className=" h-[15px] border-2" type="checkbox" />
+              <label className="font-normal text-[16px] leading-[24px] text-[#121212]">Keep me logged in</label>
             </div>
-            <div>
+            <div className="font-normal text-[16px] leading-[20px] text-primaryColor">
               <Link to="/forgot-password">Forget Password?</Link>
             </div>
           </div>
           <div className="">
-            <button className="w-[367px] h-[42px] rounded-[8px] p-[8px, 32px] border-2 bg-[#713586] mt-5 text-white" 
-             onClick={handleSubmit} 
-             >
-              Login
-            </button>
+            <PrimaryBtn
+              text="text-[13px] md:text-[16px] lg:text-[18px] text-white"
+              w="w-full"
+              onClick={handleSubmit}
+              className="mt-8"
+              isLoading={isLoading}
+            >
+              LogIn
+            </PrimaryBtn>
+
             <Link to="/register">
-              <p className="mt-2">
+              <p className="mt-2 font-medium text-[18px] leading-[27px] text-[#121212]">
                 Don't have an Account?
-                <span
-                  className="text-[#713586] cursor-pointer"
-               
-                >
-                  Sign Up
-                </span>
+                <span className="text-primaryColor cursor-pointer">Sign Up</span>
               </p>
             </Link>
           </div>
